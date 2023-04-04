b5fc3a91a2e70d1330eb53837ea313dde8615fc1
shuyu36 <sharon87306@gmail.com>
2023-04-04 16:15:18 +0800

    0404 font

---
+++ js/index.js
@@ -0,0 +1,16 @@
+$(".banner2-pic").each(function () {
+  if (window.screen.width < 428) {
+    let slideImgs = $(this).find(".banner2-pic img"),
+      slideImgsCount = slideImgs.length,
+      currentIndex = 0;
+    slideImgs.eq(currentIndex).fadeIn();
+    setInterval(showNextSlide, 5000);
+
+    function showNextSlide() {
+      let nextIndex = (currentIndex + 1) % slideImgsCount;
+      slideImgs.eq(currentIndex).fadeOut();
+      slideImgs.eq(nextIndex).fadeIn();
+      currentIndex = nextIndex;
+    }
+  }
+});
