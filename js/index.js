b06031c5f5e878b2158b5861fceaf35dd3082fb8
shuyu36 <sharon87306@gmail.com>
2023-04-08 14:03:40 +0800

    0408 pic.off

--- js/index.js
+++ js/index.js
@@ -1,16 +1,17 @@
-$(".banner2-pic").each(function () {
-  if (window.screen.width < 428) {
+$(".banner2-pic img").each(function () {
+  // if (window.screen.width < 428) 
+  // {
     let slideImgs = $(this).find(".banner2-pic img"),
       slideImgsCount = slideImgs.length,
       currentIndex = 0;
     slideImgs.eq(currentIndex).fadeIn();
-    setInterval(showNextSlide, 5000);
+    setInterval(showNextSlide, 1000);
 
     function showNextSlide() {
       let nextIndex = (currentIndex + 1) % slideImgsCount;
       slideImgs.eq(currentIndex).fadeOut();
       slideImgs.eq(nextIndex).fadeIn();
       currentIndex = nextIndex;
-    }
+    // }
   }
 });
